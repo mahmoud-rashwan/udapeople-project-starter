f1b9cced17d8788890d58460c6f3f0db02c1da13
unknown <mahmoud.sami92@outlook.com>
2022-12-25 18:21:55 +0200

    edited set_env.sh

--- backend/set_env.sh
+++ backend/set_env.sh
@@ -8,5 +8,5 @@ export TYPEORM_MIGRATIONS=./src/migrations/*.ts
 export TYPEORM_HOST=udapeople-db.c7eq7bxkt03m.us-east-1.rds.amazonaws.com
 export TYPEORM_PORT=5432
 export TYPEORM_USERNAME=postgres
-export TYPEORM_PASSWORD=GEVLyXDEbwYc4r67D9z1
+export TYPEORM_PASSWORD=eZ3boVUFMd7SC9GRgwjf
 export TYPEORM_DATABASE=glee
