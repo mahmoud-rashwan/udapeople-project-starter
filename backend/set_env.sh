9f250505a3da05512297653ec8d7153ae4b10de5
unknown <mahmoud.sami92@outlook.com>
2023-01-01 23:03:47 +0200

    packages have been updated , and env variables have been set in set_env.sh for backend

---
+++ backend/set_env.sh
@@ -0,0 +1,14 @@
+export NODE_ENV=production
+export VERSION=1
+export TYPEORM_CONNECTION=postgres
+export TYPEORM_MIGRATIONS_DIR=./src/migrations
+export TYPEORM_ENTITIES=./modules/domain/**/*.entity{.ts,.js}
+export TYPEORM_MIGRATIONS=./migrations/*{.ts,.js}
+export TYPEORM_MIGRATIONS_DIR=./migrations
+export TYPEORM_ENTITIES=./modules/domain/*/.entity.js
+export TYPEORM_MIGRATIONS=./migrations/*.js
+export TYPEORM_HOST=udapeople-db.c7eq7bxkt03m.us-east-1.rds.amazonaws.com
+export TYPEORM_PORT=5432
+export TYPEORM_USERNAME=postgres
+export TYPEORM_PASSWORD=password
+export TYPEORM_DATABASE=glee
